b72a551fa495577f05398ece137e6dcdfd177e1c
auzmartist <auz.martin@gmail.com>
2017-08-31 10:59:51 -0500

    Add mat4.multiply test

--- test/mat4.spec.js
+++ test/mat4.spec.js
@@ -29,6 +29,41 @@ describe('mat4 parity tests', () => {
     equal.mat4(s, t)
   })
 
+  it('mat4.multiply == Matrix4.multiplyMatrices', () => {
+    const a = [
+      1.0, 0.0, 0.0, 0.0,
+      0.0, 1.0, 0.0, 0.0,
+      0.0, 0.0, 1.0, 0.0,
+      2.0, 2.0, 2.0, 1.0,
+    ]
+    const b = [
+      -1.0, 0.0, 0.0, 2.0,
+      0.0, 1.0, 0.0, 2.0,
+      0.0, 0.0, -1.0, 2.0,
+      0.0, 0.0, 0.0, 1.0,
+    ]
+
+    const s = mat4.create()
+    const t = new Matrix4()
+    // Identity matrices are the same
+    equal.mat4(s, t)
+
+    const glmA = mat4.transpose([], mat4.fromValues(...a))
+    const glmB = mat4.transpose([], mat4.fromValues(...b))
+    
+    const threeA = new Matrix4().set(...a)
+    const threeB = new Matrix4().set(...b)
+
+    // New mat4.fromValues must be transposed to match ThreeJS
+    equal.mat4(glmA, threeA)
+    equal.mat4(glmB, threeB)
+
+    mat4.multiply(s, glmA, glmB)
+    t.multiplyMatrices(threeA, threeB)
+
+    equal.mat4(s,t)
+  })
+
   it('mat4.fromQuat == Matrix4.makeRotationFromQuaternion', () => {
     const quatValues = [1.0, 2.0, 3.0, 4.0]
     const sQuat = quat.fromValues(...quatValues)
@@ -42,44 +77,44 @@ describe('mat4 parity tests', () => {
     equal.mat4(s, t)
   })
 
-  it('mat4.getPosition, mat4.getScaling, mat4.getTranslation == Matrix4.decompose', () => {
-    const v = [
-      1.0, 0.0, 0.0, 5.0,
-      0.0, 1.0, 0.0, 5.0,
-      0.0, 0.0, 1.0, 5.0,
-      0.0, 0.0, 0.0, 1.0,
-    ]
+  // it('mat4.getPosition, mat4.getScaling, mat4.getTranslation == Matrix4.decompose', () => {
+  //   const v = [
+  //     1.0, 0.0, 0.0, 0.0,
+  //     1.0, 1.0, 0.0, 0.0,
+  //     0.0, 0.0, 1.0, 0.0,
+  //     0.0, 0.0, 0.0, 1.0,
+  //   ]
 
-    const s = mat4.fromValues(...v)
-    mat4.transpose(s, s)
-    let sRotation = quat.create()
-    let sTranslation = vec3.create()
-    let sScaling = vec3.create()
-    mat4.getTranslation(sTranslation, s)
-    mat4.getRotation(sRotation, s)
-    mat4.getScaling(sScaling, s)
+  //   const s = mat4.fromValues(...v)
+  //   mat4.transpose(s, s)
+  //   const sTranslation = mat4.getTranslation([], s)
+  //   const sScaling = mat4.getScaling([], s)
+  //   const sRotation = mat4.getRotation([], s)
 
-    const t = new Matrix4()
-    t.set(...v)
-    let tRotation = new Quaternion()
-    let tTranslation = new Vector3()
-    let tScaling = new Vector3()
-    t.decompose(tTranslation, tRotation, tScaling)
+  //   const t = new Matrix4()
+  //   t.set(...v)
+  //   let tRotation = new Quaternion()
+  //   let tTranslation = new Vector3()
+  //   let tScaling = new Vector3()
+  //   t.decompose(tTranslation, tRotation, tScaling)
 
-    print.quat(sRotation, 'sRotation')
-    print.quat(tRotation, 'tRotation')
+  //   print.mat(s, 's')
+  //   print.mat(t, 't')
 
-    print.vec3(sTranslation, 'sTranslation')
-    print.vec3(tTranslation, 'tTranslation')
+  //   print.quat(sRotation, 'sRotation')
+  //   print.quat(tRotation, 'tRotation')
 
-    print.vec3(sScaling, 'sScaling')
-    print.vec3(tScaling, 'tScaling')
+  //   print.vec3(sScaling, 'sScaling')
+  //   print.vec3(tScaling, 'tScaling')
 
-    equal.mat4(s, t)
-    equal.quat(sRotation, tRotation)
-    equal.vec3(sTranslation, tTranslation)
-    equal.vec3(sScaling, tScaling)
-  })
+  //   print.vec3(sTranslation, 'sTranslation')
+  //   print.vec3(tTranslation, 'tTranslation')
+
+  //   equal.mat4(s, t)
+  //   equal.quat(sRotation, tRotation)
+  //   equal.vec3(sTranslation, tTranslation)
+  //   equal.vec3(sScaling, tScaling)
+  // })
 
   it('mat4.fromRotationTranslationScale == Matrix4.compose', () => {
     const rotationValues = [0.0, 4.0, 2.0, 0.0]
